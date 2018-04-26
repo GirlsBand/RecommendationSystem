181ae754915b691a0808e9751c6aa7f6dcf23598
BETLAB\iryna.robulets <iryna.robulets@betlab.com>
2018-04-26 13:02:51 +0300

    light changes

---
+++ RecommendSystem/Models/AccesTokenInfo.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace RecommendationSystem.Models
+{
+    public class AccesTokenInfo
+    {
+        [JsonProperty("Access_Token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("Token_Type")]
+        public string TokenType { get; set; }
+    }
+}
