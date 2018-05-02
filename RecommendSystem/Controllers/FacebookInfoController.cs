0b86e65d0ad49fc4b1c711697f67e3715e34ec8d
unknown <zelikmananna2015@gmail.com>
2018-05-02 10:20:00 +0300

    Draft usage of Auth0

---
+++ RecommendSystem/Controllers/FacebookInfoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecommendationSystem.Controllers
+{
+    public class FacebookInfoController : ControllerBase
+    {
+        [Route("api/login/facebook")]
+        [HttpGet]
+        [Authorize]
+        public string GetUserInfo()
+        {
+            return "UserInfo";
+        }
+    }
+}
--- RecommendSystem/Startup.cs
+++ RecommendSystem/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +8,10 @@ namespace RecommendationSystem
 {
     public class Startup
     {
+        const string AppId = "193302211282683";
+        const string AppSecret = "2fb8063b7eded7b8a0f63b68c108dc7f";
+        const string Domain = "https://recommendation-system.auth0.com/";
+        const string Audience = "https://recommendation-system.auth0.com/api/v2/";
 
         public IConfiguration Configuration { get; }
 
@@ -21,11 +26,23 @@ namespace RecommendationSystem
         public void Configure(IApplicationBuilder app)
         {
             app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Domain;
+                options.Audience = Audience;
+            });
         }
     }
 }
