6e05c4baa4475e3740ff75818c6bf8992407a540
rinagreen0902 <37479414+rinagreen0902@users.noreply.github.com>
2018-05-09 18:40:21 +0300

    Add files via upload
    Basic Questionary http://saloedov.ml:4000/browse/REC-17

---
+++ RecommendSystem/Question.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Client
+{
+    [DataContract]
+    class Question
+    {
+        [DataMember]
+        string question;
+        [DataMember]
+        List<string> answers;
+
+        public Question()
+        {
+            question = "";
+            answers = new List<string>();
+            answers.Add("");
+        }
+
+        public Question(string questionText, List<string> answersText)
+        {
+            question = questionText;
+            answers = new List<string>();
+            foreach (string answerText in answersText)
+            {
+                answers.Add(answerText);
+            }
+        }
+    }
+}
