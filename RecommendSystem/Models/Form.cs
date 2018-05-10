b2c32a39dc891cf0f1ed08bb11b5c05f3aeeba0a
BETLAB\iryna.robulets <iryna.robulets@betlab.com>
2018-05-10 14:24:08 +0300

    add default form

---
+++ RecommendSystem/Models/Form.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace RecommendationSystem.Models
+{
+    public class Form
+    {
+        public Dictionary<string, string[]> Questions { get; }
+
+        public Form(Dictionary<string, string[]> formItems)
+        {
+            Questions = formItems;
+        }
+
+        public static Form Default => 
+            new Form(new Dictionary<string, string[]>
+            {
+                { "What do you prefer?", new []{ "City", "CountrySide"} },
+                { "Do you want to live near a subway station?", new []{ "Yes", "No"}}
+            });
+
+    }
+}
