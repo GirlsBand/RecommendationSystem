0a3a671273d48a883f8b470924331af0a4989453
iryna.robulets <irene9531q@gmail.com>
2018-05-19 14:31:53 +0300

    Add frontEnd files change survey form

--- RecommendSystem/Models/Survey.cs
+++ RecommendSystem/Models/Survey.cs
@@ -1,57 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace RecommendationSystem.Models
 {
     public class Survey
     {
-        public const string InCityPreferenceQuestion = "What do you prefer?";
+        public Question[] Questions { get; set; }
 
-        public List<Question> Questions { get; set; }    
-        public Survey(List<Question> questions)
+        public static Survey Default => new Survey
         {
-            Questions = questions;
-        }
-
-        public void AddQuestion(Question question)
-        {
-            Questions.Add(question);
-        }
-
-        public Question FindQuestion(Question question) => FindQuestion(question.Title);
-        
-
-        public Question FindQuestion(string title) => Questions.Find(question => question.Title == title);
-    
-        public void ReplaceQuestion(Question question)
-        {
-            Questions.Remove(FindQuestion(question));
-
-            AddQuestion(question);
-        }
-
-        public void AddAnswer(string title, string answer)
-        {
-            var questionToUpdate = FindQuestion(title);
-
-            if (questionToUpdate == null)
-                throw new ArgumentException("No question with such title was found");
-            if (questionToUpdate.Answers.Contains(answer))
-                throw new ArgumentException("Such answer is already present");
-
-            questionToUpdate.AddAnswer(answer);
-
-            ReplaceQuestion(questionToUpdate);
-        }
-
-        public static Survey Default
-        {
-            get
-            {
-                var firstQuestion = new Question(InCityPreferenceQuestion, new List<string> { "City", "CountrySide" });
-
-                return new Survey(new List<Question> { firstQuestion });
-            }
-        }
+            Questions = new[]
+                {
+                    new Question("What do you prefer?",
+                        new List<string> {"City", "CountrySide"})
+                }
+        };
     }
 }
